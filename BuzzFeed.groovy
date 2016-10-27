0026a3494cec00f783d6a6d4daeb67b7f7698da3
martinlander <martin_lander@t-online.de>
2016-10-27 09:00:13 +0200

    Update BuzzFeed.groovy

--- BuzzFeed.groovy
+++ BuzzFeed.groovy
@@ -5,9 +5,9 @@ def BF = []
 for(j=0;j<=99;j++) {
        BF[j] = j+1
     if ((BF[j] % 15) == 0) {
-        BF[j] = "Buzzfeed"
+        BF[j] = "FizzBuzz"
     } else if ((BF[j] % 5) == 0) {
-        BF[j] = "Feed"
+        BF[j] = "Fizz"
     } else if ((BF[j]) % 3 == 0) {
         BF[j] = "Buzz"
     }
