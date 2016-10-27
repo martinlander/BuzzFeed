a7211a377bcfa7343aad171bb202c7b0c09062f9
martinlander <martin_lander@t-online.de>
2016-10-27 08:50:04 +0200

    Update BuzzFeed.groovy
    Groovy magic

--- BuzzFeed.groovy
+++ BuzzFeed.groovy
@@ -2,15 +2,13 @@
  * Created by mlander1 on 27.10.2016.
  */
 def BF = []
-for(i=0;i<=99;i++){
-    BF[i] = i+1
-}
 for(j=0;j<=99;j++) {
+       BF[j] = j+1
     if ((BF.getAt(j) % 15) == 0) {
         BF[j] = "Buzzfeed"
-    } else if ((BF.getAt(j) % 5) == 0) {
+    } else if ((BF[j] % 5) == 0) {
         BF[j] = "Feed"
-    } else if ((BF.getAt(j)) % 3 == 0) {
+    } else if ((BF[j]) % 3 == 0) {
         BF[j] = "Buzz"
     }
 }
